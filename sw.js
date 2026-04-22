973e2be5fd4c0ca53a7ea4e25fc77077fad6b9ef
chrstopher03 <160043279+chrstopher03@users.noreply.github.com>
2026-04-21 23:17:51 -0600

    ya

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 const CACHE = "luve-v1";
 
-self.addEventListener("install", e => {
+self.addEventListener("install", e=>{
   e.waitUntil(
-    caches.open(CACHE).then(cache => {
+    caches.open(CACHE).then(cache=>{
       return cache.addAll([
         "./",
         "./index.html"
@@ -11,8 +11,8 @@ self.addEventListener("install", e => {
   );
 });
 
-self.addEventListener("fetch", e => {
+self.addEventListener("fetch", e=>{
   e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
+    caches.match(e.request).then(res=>res || fetch(e.request))
   );
 });
